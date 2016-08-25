df599edc99da3ba25b7d29d3859c8d9235118cff
Robert Korulczyk <robert@korulczyk.pl>
2016-08-25 16:52:38 +0200

    Move main menu to separate widget.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -2,11 +2,10 @@
 
 /* {licenseheader} */
 
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use app\widgets\MainMenu;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -27,35 +26,7 @@ AppAsset::register($this);
 		<?php $this->beginBody() ?>
 
 		<div class="wrap">
-			<?php
-			NavBar::begin([
-				'brandLabel' => 'My Company',
-				'brandUrl' => Yii::$app->homeUrl,
-				'options' => [
-					'class' => 'navbar-inverse navbar-fixed-top',
-				],
-			]);
-			echo Nav::widget([
-				'options' => ['class' => 'navbar-nav navbar-right'],
-				'items' => [
-					['label' => 'Home', 'url' => ['/site/index']],
-					['label' => 'About', 'url' => ['/site/about']],
-					['label' => 'Contact', 'url' => ['/site/contact']],
-					Yii::$app->user->isGuest ? (
-							['label' => 'Login', 'url' => ['/site/login']]
-							) : (
-							'<li>'
-							. Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-							. Html::submitButton(
-									'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link']
-							)
-							. Html::endForm()
-							. '</li>'
-							)
-				],
-			]);
-			NavBar::end();
-			?>
+			<?= MainMenu::widget() ?>
 
 			<div class="container">
 				<?=
